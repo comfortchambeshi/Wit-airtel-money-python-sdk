3df12be0dc53a6c21b6bc41e284e16f9ebcaaa79
Witlevels Official <31566863+comfortchambeshi@users.noreply.github.com>
2023-01-04 19:31:38 +0200

    Update call_pay.py

--- call_pay.py
+++ call_pay.py
@@ -1,14 +1,25 @@
 from classes.airtel_pay import AirtelPay
 
 #Request pay
-#pay = AirtelPay.pay("0972927679", "1", "ZMW", "ZM", "xandom-unique-id440032t")
+#pay = AirtelPay.pay("0972927679", "1", "ZMW", "ZM", "random-02345678")
 #print(pay["jsondata"])
 
-#verify = AirtelPay.verify_transaction("random-unique-id440032t")
-#print(verify)
+
+#Verify/check transaction status
+#verify = AirtelPay.verify_transaction("random-02345678")
+#trans = verify["jsondata"]["data"]["transaction"]["status"]
+
+#print(verify["jsondata"])
+#if trans == "TF":
+#    print("Your transactio was failed!")
+
+#if trans == "TS":
+#    print("Transaction was successful!")
+
+#if trans == "TP":
+#    print("Your transaction is pending!")    
 
 
-#Disburse
-
-#transfer = AirtelPay.transfermoney("airtel_phone", 1)
-#print(transfer)
+#Disburse funds
+transfer = AirtelPay.transfermoney("0972927679", "5")
+print(transfer)
